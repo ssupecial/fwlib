bc60078bffd7a990806a9493c0ba03518d70bee8
Kang-Eunsu <kanges802@g.skku.edu>
2024-11-06 17:56:43 +0900

    feat: parameterize command

--- raspberry/code/main.py
+++ raspberry/code/main.py
@@ -23,15 +23,23 @@ def on_connect(client, userdata, flags, rc):
 
 @click.command()
 @click.option("--ip", default="192.168.0.11", help="CNC Machine IP Address")
-@click.option("--port", default=8193, help="CNC Machine Port Number")
-@click.option("--mqtt_ip", default="", help="MQTT Broker IP Address")
-@click.option("--mqtt_port", default=1883, help="MQTT Broker Port Number")
+@click.option("--port", type=int, default=8193, help="CNC Machine Port Number")
+@click.option("--mqtt_ip", help="MQTT Broker IP Address", required=True)
+@click.option("--mqtt_port", type=int, default=1883, help="MQTT Broker Port Number")
 @click.option("--mqtt_topic", default="cnc/controller", help="MQTT Topic")
-@click.option("--time_interval", default=1, help="Time Interval to read data")
-@click.option("--print_log", default=False, help="Print log")
+@click.option(
+    "--time_interval",
+    type=float,
+    default=1.0,
+    help="Time Interval to read data(seconds)",
+)
+@click.option("--print_log", is_flag=True, default=False, help="Print log to console")
 def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
     if print_log:
         logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.ERROR)
+
     # MQTT Broker에 연결
     try:
         mqtt_client = mqtt.Client("CNC Machine")
@@ -42,7 +50,7 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
         logging.error(f"Failed to connect to MQTT Broker: {e}")
         raise click.ClickException(str(e))
 
-    # CNC Machine에 연결 후, 데이터 읽어옴
+    # CNC Machine에 연결 후, time interval 주기마다 데이터 읽어옴
     try:
         start_time = time.perf_counter()
         with CNCDevice(ip, port) as cnc:
@@ -73,7 +81,7 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
                     logging.error(f"Failed to read speeds: {e}")
 
                 try:
-                    # Feed rate 읽어옴
+                    # 이송 속도 읽어옴
                     feed_rate = cnc.read_feed_rate()
                     message["feed_rate"] = feed_rate
                     logging.info(f"[CNC Machine Feed Rate]\n{feed_rate}")
@@ -81,7 +89,7 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
                     logging.error(f"Failed to read feed rate: {e}")
 
                 try:
-                    # 피드레이트와 스핀들 속도를 읽어옴
+                    # 이송 속도와 스핀들 속도를 읽어옴
                     feed_rate_speed = cnc.read_feed_rate_and_speed(-1)
                     message["feed_rate_speed"] = feed_rate_speed
                     logging.info(
@@ -90,6 +98,7 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
                 except Exception as e:
                     logging.error(f"Failed to read feed rate and speed: {e}")
 
+                """cnc_rdgcode"""
                 try:
                     # 현재 활성화된 블럭의 modal Gcode를 한 번에 읽어옴
                     gcode = cnc.read_all_active_modal_g_codes()
@@ -106,6 +115,7 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
                 except Exception as e:
                     logging.error(f"Failed to read one shot gcode: {e}")
 
+                """cnc_modal"""
                 try:
                     # 현재 활성화된 블럭의 modal data를 한 번에 읽어옴
                     modal_data = cnc.read_all_active_modal_data()
@@ -137,14 +147,6 @@ def main(ip, port, mqtt_ip, mqtt_port, mqtt_topic, time_interval, print_log):
                     logging.info(f"[CNC Machine Other Data]\n{other_data}")
                 except Exception as e:
                     logging.error(f"Failed to read other data: {e}")
-
-                try:
-                    # 이전 블럭의 modal Gcode를 한 번에 읽어옴
-                    gcode_prev = cnc.read_all_previous_modal_g_codes()
-                    message["modal_gcode_prev"] = gcode_prev
-                    logging.info(f"[CNC Machine Previous Modal GCode]\n{gcode_prev}")
-                except Exception as e:
-                    logging.error(f"Failed to read previous modal gcode: {e}")
 
                 timestamp = dt.datetime.now(ZoneInfo("Asia/Seoul")).strftime(
                     "%Y-%m-%d %H:%M:%S"
