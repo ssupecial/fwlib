2110c5a1246ff4c899bb1bbfdc7acc79004ec302
Kang-Eunsu <kanges802@g.skku.edu>
2024-11-06 11:15:06 +0900

    resolve typo

--- raspberry/gcode_map.h
+++ raspberry/gcode_map.h
@@ -9,7 +9,7 @@ extern "C" {
 // 입력: type - G코드 타입 번호
 //       g_data - FOCAS에서 받은 g_data 값
 // 반환: 매핑된 G코드 문자열 (예: "G00", "G01" 등)
-extern const char* mapGCode(int type, unsigned char g_data);
+extern const char* mapGcode(int type, unsigned char g_data);
 
 #ifdef __cplusplus
 }
