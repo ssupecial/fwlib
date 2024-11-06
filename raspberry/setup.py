d1bf0932203c239a70fb94012543922e7422b1f6
Kang-Eunsu <kanges802@g.skku.edu>
2024-11-06 11:08:12 +0900

    add gcode_map.c to source code

--- raspberry/setup.py
+++ raspberry/setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, Extension
 
 module = Extension(
     "fwlib",
-    sources=["fwlib.c"],
+    sources=["fwlib.c", "gcode_map.c"],
+    include_dirs=["."],
     libraries=["fwlib32"],
 )
 
 setup(
     name="fwlib",
     version="1.0",
-    description="",
+    description="FANUC FOCAS library Python wrapper",
     ext_modules=[module],
 )
